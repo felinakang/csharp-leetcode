365a92cc0c1ed19a78a8f8a41284dc6f045919dd
Felina Kang <felinakang0123@gmail.com>
2023-04-18 14:58:18 -0700

    Create 1768 Merge Strings Alternately.cs

---
+++ 1768 Merge Strings Alternately.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Solution
+{
+    public string MergeAlternately(string word1, string word2)
+    {
+        int len = word1.Length + word2.Length;
+        int j = 0;
+        char[] word3 = new char[len];
+        for (int i = 0; i < len; i++)
+        {
+            if (j < word1.Length && j < word2.Length)
+            {
+                word3[i] = word1[j];
+                word3[i + 1] = word2[j];
+                i += 1;
+                j += 1;
+            }
+            else if (j < word1.Length && j >= word2.Length)
+            {
+                word3[i] = word1[j];
+                j += 1;
+            }
+            else if (j >= word1.Length && j < word2.Length)
+            {
+                word3[i] = word2[j];
+                j += 1;
+            }
+        }
+        return new string(word3);
+    }
+}
